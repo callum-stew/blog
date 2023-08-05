d60519a5bcbee3b7f15f57646760ab9101d58deb
Callum Stew <callum.d.stew@gmail.com>
2023-08-05 21:09:45 +0100

    added TPi resources

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Turing Pi Resources`,
+      href: "https://github.com/callum-stew/TuringPi",
     },
   ],
   // blog setting (required)
